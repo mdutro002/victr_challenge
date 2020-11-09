703e47f6650c194d83edea2803262b84358ea348
Madeline Dutro <dutro.madeline@gmail.com>
2020-11-09 13:37:13 -0500

    fleshes out main index page, adds pseudocode

--- index.php
+++ index.php
@@ -1,4 +1,34 @@
 <?php 
-  echo "hewwo";
+
+  /* Connect to DB? */
+
+  /* DATA GET */
+
+    /* API call here to grab most popular repos */
+
+    /* Sanatize Data & write to DB */
+
+  /* DATA DISPLAY */
+
+  /* Database data pull here */
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Github Allstars - PHP edition</h1>
+
+<!-- DATA WILL GO DOWN HERE - conditional formatting, etc -->
+  <?php 
+    echo "hewwwwooooooo";
+  ?>
+  
+</body>
+</html>
