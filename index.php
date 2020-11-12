5da9a4793e2a64d8a1f5505d82c4d30c5b4748c3
Madeline Dutro <dutro.madeline@gmail.com>
2020-11-11 21:31:58 -0500

    testing datatables

--- index.php
+++ index.php
@@ -74,11 +74,16 @@
   <title>PHP Repos</title>
   <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="./main.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
+  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js"></script>
+  <script>
+      $(document).ready( function () {
+        $('#table_id').DataTable();
+      } );
+  </script>
 </head>
 <body>
   <h1>Github Allstars - PHP edition</h1>
-
-
 
   <table id="table_id" class="display">
     <thead>
